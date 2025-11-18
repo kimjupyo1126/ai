d2932308689e99c929a023c971ef4aed6282d74d
kimjupyo1126 <kimjupyo1126@daum.net>
2025-11-18 18:05:12 +0900

    python 11.18

--- source/04_DBMS/1118_MySQL.sql
+++ source/04_DBMS/1118_MySQL.sql
@@ -110,11 +110,11 @@ create table personal(
     primary key(pno),
     foreign key(dno) references division(dno)
 );
+
 insert into division values (10, 'finance', '02-2088-5679','신림');
 insert into division values (20, 'research', '02-555-4321','강남');
 insert into division values (30, 'sales', '02-717-4321','마포');
 insert into division values (40, 'cs', '031-4444-4321','수원');
-select * from division;
 
 insert into personal values (1111,'smith','manager', 1001, '1990-12-17', 1000, null, 10);
 insert into personal values (1112,'ally','salesman',1116,'1991-02-20',1600,500,30);
@@ -126,50 +126,91 @@ insert into personal values (1118,'martin','analyst',1111,'1991-09-09',3450,null
 insert into personal values (1121,'kim','clerk',1114,'1990-12-08',4000,null,20);
 insert into personal values (1123,'lee','salesman',1116,'1991-09-23',1200,0,30);
 insert into personal values (1226,'park','analyst',1111,'1990-01-03',2500,null,10);
+-- workbench에서 자동 commit mode 운용
+select * from division;
 select * from personal;
 
---	select문 (and 연산자는 && 또는 and)
 -- 1. 사번, 이름, 급여를 출력
-select pno, pname, pay 
-  from personal;
+select pno, pname, pay from personal;
 
 -- 2. 급여가 2000~5000 사이 모든 직원의 모든 필드
-select * 
-  from personal 
- where pay between 2000 and 5000;
+select * from personal where pay >=2000 and pay<=5000;
+select * from personal where pay >=2000 && pay<=5000;
+select * from personal where pay between 2000 and 5000;
 
 -- 3. 부서번호가 10또는 20인 사원의 사번, 이름, 부서번호
-select pno, pname, dno 
-  from personal 
- where dno in (10,20);
+select pno, pname, dno from personal where dno in (10, 20);
 
 -- 4. 보너스가 null인 사원의 사번, 이름, 급여 급여 큰 순정렬
-select pno, pname, pay 
-  from personal 
- where bonus is null order by pay desc;
-
+select pno, pname, pay from personal where bonus is null
+	order by pay desc;
+    
 -- 5. 사번, 이름, 부서번호, 급여. 부서코드 순 정렬 같으면 PAY 큰순
-select pno, pname, dno, pay  
-  from personal
- order by pay desc;
-  
+select pno, pname, dno, pay
+	from personal order by dno, pay desc;
+    
 -- 6. 사번, 이름, 부서명
-select pno, pname, dno  
-  from personal;
-  
+select pno, pname, dname
+	from personal p, division d
+    where p.dno=d.dno;
+select pno, pname, dname
+	from personal p left join division d -- left join : 왼쪽데이터 기준
+    on p.dno=d.dno;
+    
 -- 7. 사번, 이름, 상사이름
-select pno, pname, manager  
-  from personal ;
-  
+select w.pno, w.pname, m.pname manager_name
+	from personal w, personal m
+    where w.manager = m.pno; -- 상사 있는 9명만 출력
+    
 -- 8. 사번, 이름, 상사이름(상사가 없는 사람도 출력하되 상사가 없는 경우 ★CEO★로 출력) 
-
+select w.pno, w.pname, ifnull(m.pname, '★CEO★') manager_name
+	from personal w  left outer join personal m
+    on w.manager = m.pno; 
+    
 -- 8-1 사번, 이름, 상사사번(상사가 없으면 ceo로 출력. ifnull함수의 매개변수의 타입이 상이해도 상관없음)
+select pno, pname, ifnull(manager, 'ceo') manager from personal;
 
 -- 8-2. 사번, 이름, 상사이름, 부서명(상사가 없는 사람도 출력) – 같이 합시다
+select w.pno, w.pname, ifnull(m.pname, '★CEO★') manager, dname
+	from division d, personal w  left outer join personal m
+    on w.manager = m.pno
+    where d.dno = w.dno;
 
--- 9. 이름이 s로 시작하는 사원 이름 (like 이용, substr함수이용, instr함수 이용등 다양하게 사용 가능)
-select pno, pname, manager  
-  from personal;
-  
+select w.pno, w.pname, ifnull(m.pname, '★CEO★') manager, dname
+	from (personal w, division d)  left outer join personal m
+		on w.manager = m.pno
+    where d.dno = w.dno;
+
+select w.pno, w.pname, ifnull(m.pname, '★CEO★') manager, dname
+	from personal w  left outer join personal m
+		on w.manager = m.pno, division d
+	where w.dno=d.dno;
+
+select w.pno, w.pname, ifnull(m.pname, '★CEO★') manager, dname
+	from personal w  left outer join personal m
+		on w.manager = m.pno left join division d -- 왼쪽 기준으로 
+	on w.dno=d.dno;
+    
+select w.pno, w.pname, ifnull(m.pname, '★CEO★') manager, dname
+	from personal w  left outer join personal m
+		on w.manager = m.pno join division d -- 매칭되는 거만 보여줌
+	on w.dno=d.dno;
+    
+-- 9. 이름이 s로 시작하는 사원 이름 (like 이용)
+select pname from personal where pname like 's%';
+
 -- 10. 사번, 이름, 급여, 부서명, 상사이름
+select w.pno, w.pname, w.pay, dname, m.pname manager_name
+	from division d, personal w, personal m
+    where d.dno=w.dno && w.manager=m.pno;
 
+-- oracle과 다른 함수들과 연결연산자(||)
+select pname||'은'||job from personal; -- 연결연산이 안 됨
+select concat(pname, '은', job) from personal;
+
+select sysdate(); -- mysql는 select절 가능
+-- date_format(날짜형, 포맷) : 날짜형을 문자로
+-- date_format(문자형, 포맷)  : 문자형을 날짜형으로
+	-- %Y(년도 4자리) %y(년도 2자리), %m(월) %d(일 01, 02,.), %c(일1,2)
+    -- %h(12시) %H(24시) %i(분) %s(초) %p(오전)
+select date_format(sysdate(), '%y년 %m월 %d일 %p %h:%i:%s') 지금;
